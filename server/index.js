b7276d5fd38409953cd960bd9eab6554cb778caa
Pascal Martineau <pascal@lewebsimple.ca>
2017-05-05 14:28:21 -0400

    Initial Nuxt.js integration

--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@
 const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
-  logger.info(`Feathers application started on ${app.get('host')}:${port}`)
-);
+process.on('nuxt:build:done', (err) => {
+  if (err) {
+    logger.error(err);
+  }
+  const server = app.listen(port);
+
+  server.on('listening', () => {
+    logger.info(`Feathers application started on ${app.get('host')}:${port}`);
+  });
+});
