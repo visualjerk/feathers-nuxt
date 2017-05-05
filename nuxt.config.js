fdc5b92fec2b788cd541d5b8a2e5538fa564a2b1
Pascal Martineau <pascal@lewebsimple.ca>
2017-05-05 14:42:45 -0400

    Initial Bootstrap v4 integration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,23 @@
+const webpack = require('webpack');
+
 module.exports = {
+  build: {
+    vendor: ['jquery', 'tether', 'bootstrap'],
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        Tether: 'tether',
+        'window.jQuery': 'jquery',
+        'window.Tether': 'tether'
+      })
+    ]
+  },
+  css: [
+    {src: '~assets/scss/style.scss', lang: 'scss'},
+  ],
   dev: process.env.NODE_ENV !== 'production',
+  plugins: [
+    {src: '~plugins/bootstrap.js'}
+  ]
 };
